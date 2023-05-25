5efd91e2a314a5fd26a54926cf21e021c654aff2
Giuseppe Desiato <giodesiato@gmail.com>
2023-05-25 11:09:35 -0400

    services system

--- src/main/java/com/example/demo/controllers/ServicesController.java
+++ src/main/java/com/example/demo/controllers/ServicesController.java
@@ -5,9 +5,7 @@ import com.example.demo.repositories.ServicesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,10 +23,49 @@ public class ServicesController {
         return "services";
     }
 
+    @GetMapping("/new")
+    public String showAddServiceForm(Model model) {
+        Services service = new Services();
+        model.addAttribute("service", service);
+        return "add-service";
+    }
+
+    @PostMapping
+    public String addService(@ModelAttribute("service") Services service) {
+        servicesRepository.save(service);
+        return "redirect:/services";
+    }
+
     @GetMapping("/{id}")
     public String getService(@PathVariable Long id, Model model) {
         Services service = servicesRepository.findById(id).orElse(null);
         model.addAttribute("service", service);
         return "service";
+    }
+
+
+    @GetMapping("/{id}/update")
+    public String showUpdateServiceForm(@PathVariable Long id, Model model) {
+        Services service = servicesRepository.findById(id).orElse(null);
+        model.addAttribute("service", service);
+        return "update-service";
+    }
+
+    @PostMapping("/{id}/update")
+    public String updateService(@PathVariable Long id, @ModelAttribute("service") Services updatedService) {
+        Services service = servicesRepository.findById(id).orElse(null);
+        if (service != null) {
+            service.setServiceName(updatedService.getServiceName());
+            service.setDescription(updatedService.getDescription());
+            service.setCost(updatedService.getCost());
+            servicesRepository.save(service);
+        }
+        return "redirect:/services";
+    }
+
+    @RequestMapping(value = "/{id}/delete", method = {RequestMethod.GET, RequestMethod.POST})
+    public String deleteService(@PathVariable Long id) {
+        servicesRepository.deleteById(id);
+        return "redirect:/services";
     }
 }
