92a00280a45e530257dc550ac4e14cdcc7b33fa5
Giuseppe Desiato <giodesiato@gmail.com>
2024-07-23 09:43:46 +0200

    user controller tes

--- src/test/java/com/desiato/whitecanvas/controller/UserControllerTest.java
+++ src/test/java/com/desiato/whitecanvas/controller/UserControllerTest.java
@@ -5,6 +5,8 @@ import com.desiato.whitecanvas.dto.AuthenticatedUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -36,6 +38,37 @@ public class UserControllerTest extends BaseTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(authenticatedUser.user().getId()))
                 .andExpect(jsonPath("$.email").value(authenticatedUser.user().getEmail()));
+    }
+
+    @Test
+    public void createUser_ShouldHandleUserAlreadyExists() throws Exception {
+        String email = testAuthenticationHelper.generateUniqueEmail();
+        String password = "password123";
+
+        String userJson = String.format("{ \"email\": \"%s\", \"password\": \"%s\" }", email, password);
+
+        // First attempt should succeed
+        mockMvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isCreated());
+
+        // Second attempt with the same email should fail with 409 Conflict
+        mockMvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isConflict())
+                .andExpect(content().string("User already exists"));
+    }
+
+    @Test
+    public void createAndAuthenticateUser_ShouldReturnAuthenticatedUser() throws Exception {
+        AuthenticatedUser authenticatedUser = testAuthenticationHelper.createAndAuthenticateUser();
+
+        assertNotNull(authenticatedUser);
+        assertNotNull(authenticatedUser.user());
+        assertNotNull(authenticatedUser.userToken());
+        assertEquals("user_", authenticatedUser.user().getEmail().substring(0, 5));
     }
 
 }
