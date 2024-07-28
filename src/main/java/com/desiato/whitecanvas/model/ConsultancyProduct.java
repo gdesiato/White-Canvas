56a73214171531746c36f8f72932b226a7a903cb
Giuseppe Desiato <giodesiato@gmail.com>
2024-07-28 11:59:42 +0200

    consultancy product update

--- src/main/java/com/desiato/whitecanvas/model/ConsultancyProduct.java
+++ src/main/java/com/desiato/whitecanvas/model/ConsultancyProduct.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public enum ConsultancyProduct {
     COLOR_ANALYSIS("Color Analysis", new BigDecimal("150.00")),
     BODY_SHAPE("Body Shape", new BigDecimal("100.00")),
-    FACIAL_SHAPE("Facial Shape", new BigDecimal("300.00"));
+    FACIAL_SHAPE("Facial Shape", new BigDecimal("100.00"));
 
     private final String serviceName;
     private final BigDecimal price;
