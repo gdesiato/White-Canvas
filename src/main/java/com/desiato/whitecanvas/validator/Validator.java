c334c83be79565876253dd45332b5b63afa49fdb
Giuseppe Desiato <giodesiato@gmail.com>
2024-09-23 22:09:11 +0200

    class validator

--- src/main/java/com/desiato/whitecanvas/validator/Validator.java
+++ src/main/java/com/desiato/whitecanvas/validator/Validator.java
@@ -1,4 +1,25 @@
 package com.desiato.whitecanvas.validator;
 
-public interface Validator {
+import com.desiato.whitecanvas.dto.UserRequestDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Validator {
+
+    public List<String> validateUserRequestDto(UserRequestDto userRequestDto) {
+        List<String> errorMessages = new ArrayList<>();
+
+        if (userRequestDto.email() == null || userRequestDto.email().isBlank()) {
+            errorMessages.add("Email is required");
+        } else if (!userRequestDto.email().contains("@")) {
+            errorMessages.add("Invalid email format");
+        }
+
+        if (userRequestDto.password() == null || userRequestDto.password().isBlank()) {
+            errorMessages.add("Password is required");
+        }
+
+        return errorMessages;
+    }
 }
