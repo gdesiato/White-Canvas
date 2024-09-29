af420ebc510bc72d4409b2a3cbfebacf7caa0c88
Giuseppe Desiato <giodesiato@gmail.com>
2024-09-29 13:47:41 +0200

    line 77 in securityConfig fixed

--- src/main/java/com/desiato/whitecanvas/configuration/SecurityConfig.java
+++ src/main/java/com/desiato/whitecanvas/configuration/SecurityConfig.java
@@ -74,7 +74,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
