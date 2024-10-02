c9f08bba295710791e64f4832ecfb363fb6d2851
Giuseppe Desiato <giodesiato@gmail.com>
2024-10-02 09:27:42 +0200

    frontend module app

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent {
 
 }
--- frontend/src/app/services/cartService/cart.service.spec.ts
+++ frontend/src/app/services/cartService/cart.service.spec.ts
@@ -14,3 +14,4 @@ describe('CartService', () => {
     expect(service).toBeTruthy();
   });
 });
+ 
