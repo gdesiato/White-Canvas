18ecb75e66f33c70523d48a589d51dfda004d167
Giuseppe Desiato <giodesiato@gmail.com>
2024-10-09 12:54:30 +0200

    tests added to UserRequestValidatorTest

--- src/test/java/com/desiato/whitecanvas/validator/UserRequestValidatorTest.java
+++ src/test/java/com/desiato/whitecanvas/validator/UserRequestValidatorTest.java
@@ -37,4 +37,61 @@ class UserRequestValidatorTest extends BaseTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errors[0].message").value("Password cannot be blank"));
     }
+
+    @Test
+    void createUser_ShouldReturnValidationErrorWhenEmailIsBlank() throws Exception {
+
+        String userRequestJson = ("""
+        {
+          "email": "",
+          "password": "password123"
+        }
+        """);
+
+        mockMvc.perform(post("/api/user")
+                        .header("Authorization", "Bearer " + authenticatedUser.userToken().value())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].message").value("Email cannot be blank"));
+    }
+
+    @Test
+    void createUser_ShouldReturnValidationErrorWhenEmailIsInvalid() throws Exception {
+        String email = "incorrectMail.com";
+
+        String userRequestJson = String.format("""
+        {
+          "email": "%s",
+          "password": "password123"
+        }
+        """, email);
+
+        mockMvc.perform(post("/api/user")
+                        .header("Authorization", "Bearer " + authenticatedUser.userToken().value())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].message").value("Invalid email format"));
+    }
+
+    @Test
+    void createUser_ShouldReturnValidationErrorWhenEmailAndPasswordAreBlank() throws Exception {
+
+        String userRequestJson = ("""
+        {
+          "email": "",
+          "password": ""
+        }
+        """);
+
+        mockMvc.perform(post("/api/user")
+                        .header("Authorization", "Bearer " + authenticatedUser.userToken().value())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userRequestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].message").value("Email cannot be blank"))
+                .andExpect(jsonPath("$.errors[1].message").value("Password cannot be blank"));
+    }
+
 }
